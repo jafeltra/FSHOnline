b8afa22bf5ddfd40d84255afe3784e5f793ce01e
Julia Afeltra <jafeltra@mitre.org>
2021-01-19 11:02:19 -0500

    Try manual hash

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/FSHOnline">
+    <BrowserRouter basename="/FSHOnline/#">
       <AppRouter />
     </BrowserRouter>
   </React.StrictMode>,
